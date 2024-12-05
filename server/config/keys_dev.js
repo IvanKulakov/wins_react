1f9514b65e6dbe53420308b8b7a86b27acfede20
Иван Кулаков <vn8007@gmail.com>
2024-12-05 11:19:59 +0200

    server

---
+++ server/config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://TheBest:MJGiQK8BtmuEC6i@cluster0.5qewy.mongodb.net/madebyhands?retryWrites=true&w=majority',
+  secretOrKey: 'random very very secret string',
+};
